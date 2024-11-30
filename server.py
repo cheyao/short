780dba27c9308b4f507f07c3c0f3172ac4d5aadd
cheyao <94928179+cheyao@users.noreply.github.com>
2024-11-30 12:11:29 +0100

    Start of auth

--- server.py
+++ server.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from requests import get
 
 app = FastAPI();
 
-@app.get('/upload')
-def results():
-    return results
+class Item(BaseModel):
+    name: str
+    password: str
+
+def generateNonce():
+    return '1';
+
+@app.put('/upload')
+async def results(item: Item):
+    url = f"https://api.yubico.com/wsapi/2.0/verify?id=${105538}&otp=${item.password}&nonce=${generateNonce()}"; 
+    resp = get(url);
+
+    if (resp.status_code != 200):
+        return HTTPException(status_code=resp.status_code, detail="Failed to reach yubico");
+
+    if (resp.json()['status'] != "OK"):
+        return HTTPException(status_code=403, detail=resp.json()['status']);
+
