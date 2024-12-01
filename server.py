f2984e5535eb57f94ccf02d6321fc74e95ed95f0
cheyao <94928179+cheyao@users.noreply.github.com>
2024-12-01 11:31:21 +0100

    Style plus download

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, HTTPException, UploadFile, Form
+from fastapi import FastAPI, UploadFile, Form
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import aiofiles
-from typing import Union
 from typing_extensions import Annotated
 from requests import get
 import random
 import string
+from threading import Thread, Lock
 
+mutex = Lock()
 app = FastAPI();
 
 class Item(BaseModel):
@@ -14,11 +16,15 @@ class Item(BaseModel):
     password: str
     file: UploadFile
 
+stat = "";
+
 def generateNonce() -> str:
     len: int = random.randint(16, 40);
     return ''.join(random.choices(string.ascii_letters + string.digits, k=len));
 
-def auth(id: int, otp: str, retry: bool = False) -> bool | HTTPException:
+def auth(id: int, otp: str, retry: bool = False) -> bool:
+    global stat;
+
     nonce: str = generateNonce();
     url: str = f"https://api.yubico.com/wsapi/2.0/verify?id={id}&otp={otp}&nonce={nonce}"; 
     resp = get(url);
@@ -27,7 +33,8 @@ def auth(id: int, otp: str, retry: bool = False) -> bool | HTTPException:
         return auth(id, otp, True);
 
     if (resp.status_code != 200):
-        return HTTPException(status_code=resp.status_code, detail="Failed to reach yubico");
+        stat = f"HTTP {resp.status_code}: Failed to reach yubico";
+        return False;
 
     respl = [i.split('=', 1) for i in resp.text.strip().split('\n')];
     resp = {key.strip(): val.strip() for [key, val] in respl};
@@ -36,34 +43,44 @@ def auth(id: int, otp: str, retry: bool = False) -> bool | HTTPException:
         return auth(id, otp, True);
 
     if (resp['status'] != "OK"):
-        return HTTPException(status_code=403, detail=resp['status']);
+        stat = f"HTTP 403: {resp['status']}";
+        return False;
 
     if (resp['sl'] != "100"):
-        return HTTPException(status_code=403, detail=f"Only {resp['sl']}% of servers validated");
+        stat = f"HTTP 403: Only {resp['sl']}% of servers validated";
+        return False;
 
-    return True
+    return True;
 
-async def upload(file: UploadFile, name: str):
-    async with aiofiles.open(name, 'wb') as ofile:
+async def upload(file: UploadFile, name: str) -> None:
+    async with aiofiles.open(f"files/{name}", 'wb') as ofile:
         contents = await file.read();
         await ofile.write(contents);
 
-    return "Success";
-
 @app.post('/upload')
 async def results(password: Annotated[str, Form()], file: UploadFile, name: Annotated[str, Form()]):
-    result1 = auth(105543, password); # Main Yubikey
-    if type(result1) is bool and result1 == True:
-        return await upload(file, name);
+    global stat;
+    stat = ""
 
-    result2 = auth(105538, password); # Back Yubikey
-    if type(result2) is bool and result2 == True:
-        return await upload(file, name);
+    if auth(105543, password) == True:
+        await upload(file, name);
+        stat = "Success";
+        return;
 
-    if type(result1) is HTTPException:
-        return result1;
+    if auth(105538, password) == True:
+        await upload(file, name);
+        stat = "Success";
+        return;
 
-    if type(result2) is HTTPException:
-        return result2;
+@app.get('/status')
+async def status():
+    return {"status": stat};
 
+@app.get('/')
+async def index():
+    return FileResponse("index.html")
+
+@app.get('/{file}')
+async def file(file: str):
+    return FileResponse(f"files/{file}")
 
